064189d620b7b7933fbf77ecda899d7b47d8fdde
becker63 <johnsontaylor6320@gmail.com>
2025-09-30 00:13:22 -0400

    fancy refactor

--- tests/test_nftnl.nim
+++ tests/test_nftnl.nim
@@ -14,15 +14,17 @@ suite "nftnl basics":
     var r = Rule.create()
     check not r.raw.isNil
 
+  test "rset alloc/free":
+    var s = Set.create()
+    check not s.raw.isNil
+
 suite "nftnl iterators (stubbed)":
   test "empty table list iterates safely":
     var list = TableList.create()
-
     var count = 0
     for table in tableIter(list):
-      discard table # should not enter here
+      discard table
       inc count
-
     check count == 0
 
   test "empty chain list iterates safely":
@@ -49,13 +51,68 @@ suite "nftnl iterators (stubbed)":
       inc count
     check count == 0
 
-suite "test table attr casting is correct":
-  test "set/get string attr works":
-    var t = Table.create()
-    t.setAttr(NFTNL_TABLE_NAME, "mytable")
-    check t.getAttr(NFTNL_TABLE_NAME) == "mytable" # string
+suite "nftnl attr typing compile time":
+  test "string attrs accept only string":
+    static:
+      var t: Table
+      doAssert compiles(t.setAttr(NFTNL_TABLE_NAME, "ok"))
+      doAssert compiles(t.getAttr(NFTNL_TABLE_NAME))
+      doAssert not compiles(t.setAttr(NFTNL_TABLE_NAME, 123))
 
-  test "set/get family works":
+  test "uint32 attrs accept only integer types":
+    static:
+      var t: Table
+      # use plain Nim literals, not AF_INET (C consts can't be folded at CT)
+      doAssert compiles(t.setAttr(NFTNL_TABLE_FAMILY, 1'u32))
+      doAssert compiles(t.getAttr(NFTNL_TABLE_FAMILY))
+      doAssert not compiles(t.setAttr(NFTNL_TABLE_FAMILY, "nope"))
+
+  test "uint64 attrs accept only 64-bit ints":
+    static:
+      var t: Table
+      # literal must be explicitly suffixed
+      doAssert compiles(t.setAttr(NFTNL_TABLE_HANDLE, 42'u64))
+      doAssert compiles(t.getAttr(NFTNL_TABLE_HANDLE))
+      doAssert not compiles(t.setAttr(NFTNL_TABLE_HANDLE, "wrong"))
+
+suite "nftnl attr typing":
+  test "string attrs roundtrip":
     var t = Table.create()
-    t.setAttr(NFTNL_TABLE_FAMILY, AF_INET)
+    t.setAttr(NFTNL_TABLE_NAME, "roundtrip")
+    check t.getAttr(NFTNL_TABLE_NAME) == "roundtrip"
+
+  test "uint32 attrs roundtrip":
+    var t = Table.create()
+    t.setAttr(NFTNL_TABLE_FAMILY, AF_INET) # ergonomic with C constant
     check t.getAttr(NFTNL_TABLE_FAMILY) == AF_INET
+
+  test "uint64 attrs roundtrip":
+    var t = Table.create()
+    t.setAttr(NFTNL_TABLE_HANDLE, 12345) # ergonomic
+    check t.getAttr(NFTNL_TABLE_HANDLE) == 12345'u64
+
+suite "nftnl props sugar":
+  test "table.name roundtrip":
+    var t = Table.create()
+    t.name = "viaProps"
+    check t.name == "viaProps"
+
+  test "table.family roundtrip":
+    var t = Table.create()
+    t.family = AF_INET
+    check t.family == AF_INET
+
+  test "table.flags roundtrip":
+    var t = Table.create()
+    t.flags = 7
+    check t.flags == 7'u32
+
+  test "table.handle roundtrip":
+    var t = Table.create()
+    t.handle = 99999'u64
+    check t.handle == 99999'u64
+
+  test "table.userdata roundtrip":
+    var t = Table.create()
+    t.userdata = "u-data"
+    check t.userdata == "u-data"
